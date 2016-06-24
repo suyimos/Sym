dad878b9dab4336c9c75dc8c40f7c13e4541f765
张树朋 <zhangshupeng@tuyuabc.com>
2016-06-24 10:55:21 +0800

    #mark --- 页面跳转

---
+++ SymTst/SymTst/Snake/SYSnakeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYSnakeViewController.h
+//  SymTst
+//
+//  Created by zhangshupeng on 16/6/24.
+//  Copyright © 2016年 nanw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYSnakeViewController : UIViewController
+
+@end
---
+++ SymTst/SymTst/Snake/SYSnakeViewController.m
@@ -0,0 +1,13 @@
+//
+//  SYSnakeViewController.m
+//  SymTst
+//
+//  Created by zhangshupeng on 16/6/24.
+//  Copyright © 2016年 nanw. All rights reserved.
+//
+
+#import "SYSnakeViewController.h"
+
+@implementation SYSnakeViewController
+
+@end
