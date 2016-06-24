1fd6f8c7776da08406b0a5bf3f43f51aa07f8f6b
lulu1125 <1572381686@qq.com>
2016-06-24 09:45:19 +0800

    no message

--- SymTst/SymTst/ViewController.m
+++ SymTst/SymTst/ViewController.m
@@ -25,9 +25,7 @@
     NSLog(@"shishi ");
     // Do any additional setup after loading the view,  from a nib.
     
-    
-    
->>>>>>> d68ed23764c3ae4b58e6323dc7477eb34f5491e5
+
 }
 
 - (void)didReceiveMemoryWarning {
